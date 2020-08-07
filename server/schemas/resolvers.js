ae5bea89e67f06b0961713da0682cd8c26c7944e
Diab <emailcodydiab@gmail.com>
2020-08-07 07:37:19 -0500

    set up backend directory

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { User } = require('../models');
+const { AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+
+//resolvers here
+
+
+};
+
+module.exports = resolvers;
